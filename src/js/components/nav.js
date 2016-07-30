7072fb424088b4d5fd43ce7453806613db2a7fcd
Rachel Rose Ulgado <rachel.ulgado@gmail.com>
2016-07-29 17:53:01 -0700

    made a change to divider style

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -8,11 +8,11 @@ export default class Nav extends Component {
 				<div className='nav-inner-container'>
 					<ul className='nav-links'>
 						<Link to='/about' className='nav-content nav-link'>About</Link>
-						<li className='nav-content divider'>/</li>
+						<li className='nav-content divider'>|</li>
 						<Link to='/work' className='nav-content nav-link'>Work</Link>
-						<li className='nav-content divider'>/</li>
+						<li className='nav-content divider'>|</li>
 						<Link to='/resume' className='nav-content nav-link'>Resume</Link>
-						<li className='nav-content divider'>/</li>
+						<li className='nav-content divider'>|</li>
 						<Link to='/contact' className='nav-content nav-link'>Contact</Link>
 					</ul>
 				</div>
