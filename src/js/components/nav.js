9fbc02e81d954f8b88555b853025ac923c098dd4
Rachel Rose Ulgado <rachel.ulgado@gmail.com>
2016-08-02 14:39:58 -0700

    more fixes to resume and nav

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -8,11 +8,11 @@ export default class Nav extends Component {
 				<div className='nav-inner-container'>
 					<ul className='nav-links'>
 						<Link to='/about' activeClassName='nav-link-active' className='nav-content nav-link'>about</Link>
-						<li className='nav-content divider'>|</li>
+						<li className='nav-content divider'>/</li>
 						<Link to='/work' activeClassName='nav-link-active' className='nav-content nav-link'>work</Link>
-						<li className='nav-content divider'>|</li>
+						<li className='nav-content divider'>/</li>
 						<Link to='/resume' activeClassName='nav-link-active' className='nav-content nav-link'>resume</Link>
-						<li className='nav-content divider'>|</li>
+						<li className='nav-content divider'>/</li>
 						<Link to='/contact' activeClassName='nav-link-active' className='nav-content nav-link'>contact</Link>
 					</ul>
 				</div>
--- src/js/components/resume.js
+++ src/js/components/resume.js
@@ -10,14 +10,16 @@ export default class Resume extends Component {
 					<h3>Education</h3>
 				</div>
 				<div className='content'>
-					<p>M.S. Human Centered Design & Engineering</p>
-					<p>University of Washington</p><br/>
-					<p>Student</p>
-					<p>School for Poetic Computation</p><br/>
-					<p>B.S. Informatics (Human–Computer Interaction Specialization)</p>
-					<p>University of California Irvine</p><br/>
-					<p>B.A. Anthropology</p>
+					<p className='emphasize'>M.S. Human Centered Design & Engineering</p>
+					<p>University of Washington</p>
+					<p>2013 - 2015</p><br/>
+					<p className='emphasize'>Student</p>
+					<p>School for Poetic Computation</p>
+					<p>Fall 2014</p><br/>
+					<p className='emphasize'>B.S. Informatics (Human–Computer Interaction Specialization)</p>
+					<p className='emphasize'>B.A. Anthropology</p>
 					<p>University of California Irvine</p>
+					<p>2008 - 2013</p>
 				</div>
 			</div>
 		);
@@ -30,17 +32,17 @@ export default class Resume extends Component {
 					<h3>Experience</h3>
 				</div>
 				<div className='content'>
-					<p>Design Technologist Apprentice</p>
-					<p>Junior</p><br/>
-					<p>Creative Technologist in Residence</p>
+					<p className='emphasize'>Design Technologist Apprentice</p>
+					<p>Junior, San Francisco, CA</p><br/>
+					<p className='emphasize'>Creative Technologist in Residence</p>
 					<p>Schema Design</p><br/>
-					<p>UX Design Graduate Intern</p>
+					<p className='emphasize'>UX Design Graduate Intern</p>
 					<p>Intel</p><br/>
-					<p>Graduate Researcher</p>
+					<p className='emphasize'>Graduate Researcher</p>
 					<p>University of Washington</p><br/>
-					<p>UX Design Intern</p>
+					<p className='emphasize'>UX Design Intern</p>
 					<p>IBM</p><br/>
-					<p>Undergraduate Researcher</p>
+					<p className='emphasize'>Undergraduate Researcher</p>
 					<p>University of California Irvine</p>
 				</div>
 			</div>
@@ -83,11 +85,11 @@ export default class Resume extends Component {
 					<h3>Publications</h3>
 				</div>
 				<div className='content'>
-					<p>Fox, S., Ulgado, R. R., & Rosner, D. (2015, February). Hacking Culture, Not Devices: Access and Recognition in Feminist Hackerspaces. In Proceedings of the 18th ACM Conference on Computer Supported Cooperative Work & Social Computing (pp. 56-68). ACM.</p><br/>
-					<p>Ulgado, R. R., Nguyen, K., Custodio, V. E., Waterhouse, A., Weiner, R., & Hayes, G. (2013, June). VidCoach: A mobile video modeling system for youth with special needs. In Proceedings of the 12th International Conference on Interaction Design and Children (pp. 581-584). ACM.</p><br/>
-					<p>Hayes, G.R., Custodio, V.E., Haimson, O.L., Nguyen, K., Ringland, K.E., Ulgado, R.R., Waterhouse, A., and Weiner, R. Mobile video modeling for employment interviews for individuals with autism. Journal of Vocational Rehabilitation.</p><br/>
-					<p>Nguyen, K., Custodio, V.E., Ulgado, R.R., Weiner, R., Waterhouse, A., O’Neal, L., and Hayes, G.R. Evaluation of the Use of Mobile Video Modeling for Job Interviews. 2014 International Meeting for Autism Research.</p><br/>
-					<p>Ulgado, R. R. “On Imaginary Real Utopia.” Critical Theory of Technology Zine by The School for Poetic Computation, Fall 2014.</p>
+					<p>Fox, S., Ulgado, R. R., & Rosner, D. (2015, February). <a href='http://dl.acm.org/citation.cfm?id=2675223' target='_blank'> Hacking Culture, Not Devices: Access and Recognition in Feminist Hackerspaces.</a> In Proceedings of the 18th ACM Conference on Computer Supported Cooperative Work & Social Computing (pp. 56-68). ACM.</p><br/>
+					<p>Ulgado, R. R., Nguyen, K., Custodio, V. E., Waterhouse, A., Weiner, R., & Hayes, G. (2013, June). <a href='http://dl.acm.org/citation.cfm?id=2485870' target='_blank'> VidCoach: A mobile video modeling system for youth with special needs.</a> In Proceedings of the 12th International Conference on Interaction Design and Children (pp. 581-584). ACM.</p><br/>
+					<p>Hayes, G.R., Custodio, V.E., Haimson, O.L., Nguyen, K., Ringland, K.E., Ulgado, R.R., Waterhouse, A., and Weiner, R. <a href='http://content.iospress.com/articles/journal-of-vocational-rehabilitation/jvr775' target='_blank'>Mobile video modeling for employment interviews for individuals with autism. </a>Journal of Vocational Rehabilitation.</p><br/>
+					<p>Nguyen, K., Custodio, V.E., Ulgado, R.R., Weiner, R., Waterhouse, A., O’Neal, L., and Hayes, G.R. <a href='https://www.researchgate.net/publication/268143717_Evaluation_of_the_Use_of_Mobile_Video_Modeling_for_Job_Interviews' target='_blank'>Evaluation of the Use of Mobile Video Modeling for Job Interviews.</a> 2014 International Meeting for Autism Research.</p><br/>
+					<p>Ulgado, R. R. <a href='https://github.com/allisonburtch/Critical-Theory-of-Technology/blob/master/zine.pdf' target="_blank">“On Imaginary Real Utopia.”</a> Critical Theory of Technology Zine by The School for Poetic Computation, Fall 2014.</p>
 				</div>
 			</div>
 		);
@@ -100,13 +102,13 @@ export default class Resume extends Component {
 					<h3>Conferences, Festivals, Workshops</h3>
 				</div>
 				<div className='content'>
-					<p>CODAME Art + Tech Festival, San Francisco, CA.</p>
+					<p className='emphasize'>CODAME Art + Tech Festival, San Francisco, CA.</p>
 					<p>Installation Artist</p><br/>
-					<p>Workshop on Socially Engaged Arts Practice in HCI, CHI 2014, Toronto.</p>  
+					<p className='emphasize'>Socially Engaged Arts Practice in HCI, CHI 2014, Toronto.</p>  
 					<p>Workshop Participant</p><br/>
-					<p>Eyeo Festival, 2013</p>
+					<p className='emphasize'>Eyeo Festival, 2013.</p>
 					<p>Student Volunteer</p><br/> 
-					<p>Workshop on Designing for Children with Special Needs in Multiple Settings
+					<p className='emphasize'>Designing for Children with Special Needs in Multiple Settings.
 	IDC 2013</p>  
 					<p>Workshop Participant</p>
 				</div>
