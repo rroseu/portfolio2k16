5654c8f50f3e2ec64ef699f73a34e4cfdb8c6a09
Rachel Rose Ulgado <rachel.ulgado@gmail.com>
2016-08-18 13:39:42 -0700

    adding breakpoints and styling for diff sizes

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -3,20 +3,20 @@ import { Link } from 'react-router';
 
 export default class Nav extends Component {
 	render() {
-    console.log(this.props.currentPath);
-
 		return (
 			<div className='nav-container-outer'>
 				<div className='nav-container-inner' id={this.props.currentPath}>
-					<ul className='nav-links'>
-						<Link to='about' activeClassName='nav-link-active' className='nav-link'>About</Link>
-						<li className='nav-divider'>/</li>
-						<Link to='work' activeClassName='nav-link-active' className='nav-link'>Work</Link>
-						<li className='nav-divider'>/</li>
-						<Link to='resume' activeClassName='nav-link-active' className='nav-link'>Resume</Link>
-						<li className='nav-divider'>/</li>
-						<Link to='contact' activeClassName='nav-link-active' className='nav-link'>Contact</Link>
-					</ul>
+					<div className='ul-container'>
+						<ul className='nav-links'>
+							<Link to='about' activeClassName='nav-link-active' className='nav-link'>About</Link>
+							<li className='nav-divider'>/</li>
+							<Link to='work' activeClassName='nav-link-active' className='nav-link'>Work</Link>
+							<li className='nav-divider'>/</li>
+							<Link to='resume' activeClassName='nav-link-active' className='nav-link'>Resume</Link>
+							<li className='nav-divider'>/</li>
+							<Link to='contact' activeClassName='nav-link-active' className='nav-link'>Contact</Link>
+						</ul>
+					</div>
 				</div>
 			</div>
 		)
