eaaebffc2fb4dd6979c25bf697ece5cb55875a5f
Rachel Rose Ulgado <rachel.ulgado@gmail.com>
2016-10-04 14:41:42 -0700

    fixing about page

--- src/js/components/about.js
+++ src/js/components/about.js
@@ -5,8 +5,13 @@ export default class About extends Component {
 		return (
 			<div className='about-container-outer'>
 				<div className='about-container-inner'>
-					<h2>Hi, I'm Rachel.</h2>
-					<p>I'm a front-end developer living in the bay area with a background in human–computer interaction & UX research. I experiment with code and interactive media to imagine new ways of engaging with the world.</p>
+					<div className='about-left'>
+						<h2>Hi, I'm Rachel.</h2>
+						<h3>I'm a creative technologist.</h3>
+					</div>
+					<div className='about-right'>
+						<p>My background is in human–computer interaction, and human-centered design, and qualitative UX research. I experiment with code and interactive media to imagine new ways of engaging with the world. <br /><br />Previously I worked at Junior, Schema Design, Intel, and IBM, and was a student at the School for Poetic Computation in New York.</p>
+					</div>
 				</div>
 			</div>
 		);
