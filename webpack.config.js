823dd8dfd297d04ea98933686aae9d4ce8a007fc
Rachel Rose Ulgado <rachel.ulgado@gmail.com>
2016-10-05 15:38:23 -0700

    using image webpack loader for minifying assets

--- webpack.config.js
+++ webpack.config.js
@@ -15,45 +15,48 @@ const config = {
 		// for hot style updates
 		// 'webpack/hot/only-dev-server',
 		mainPath
-	],
-	output: {
-		path: buildPath,
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js?$/,
-				exclude: [nodeModulesPath],
-				loader: 'react-hot!babel'
-			},
-			{
-				test: /\.scss$/,
-				loaders: ["style", "css", "sass"]
-			},
-			{
-				test: /\.svg$/,
-				loader: 'file'
-			}
-		]
-	},
-	resolve: {
-		extensions: ['', '.js']
-	},
-	devServer: {
-		historyApiFallback: true,
-		contentBase: './dist',
-		hot: true
-	},
-	plugins: [
+		],
+		output: {
+			path: buildPath,
+			publicPath: '/',
+			filename: 'bundle.js'
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.js?$/,
+					exclude: [nodeModulesPath],
+					loader: 'react-hot!babel'
+				},
+				{
+					test: /\.scss$/,
+					loaders: ['style', 'css', 'sass']
+				},
+				{
+					test: /\.(jpe?g|png|gif|svg)$/i,
+					loaders: [
+						'file?hash=sha512&digest=hex&name=[hash].[ext]',
+						'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+					]
+				}
+			]
+		},
+		resolve: {
+			extensions: ['', '.js']
+		},
+		devServer: {
+			historyApiFallback: true,
+			contentBase: './dist',
+			hot: true
+		},
+		plugins: [
 		// new webpack.HotModuleReplacementPlugin(),
 		new webpack.DefinePlugin({
 			'process.env': {
-      	'NODE_ENV': JSON.stringify('production')
-    	}
+				'NODE_ENV': JSON.stringify('production')
+			}
 		})
-	]
-};
+		]
+	};
 
-module.exports = config;
+	module.exports = config;
