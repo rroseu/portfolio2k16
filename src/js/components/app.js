e5bf5304b62732564ab27a75eb30320f264b4c1a
Rachel Rose Ulgado <rachel.ulgado@gmail.com>
2016-10-05 14:27:32 -0700

    fixing nav for mobile, this could use more work but it should be okay for now.

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -7,10 +7,21 @@ export default class App extends Component {
 		return (
 			<div className='header-container-outer'>
 				<div className='page header'>
-					<Link to='/'>
-						<h2>Rachel Rose Ulgado</h2>
-						<div className="page header-divider"></div>
-					</Link>
+					<div className='header-left'>
+						<Link to='/'>
+							<h2>Rachel Rose Ulgado</h2>
+							<div className="page header-divider"></div>
+						</Link>
+					</div>
+					<div className='header-right'>
+						<ul className='nav-links'>
+							<Link to='about' activeClassName='nav-link-active' className='nav-link'>ABOUT</Link>
+							<li className='nav-divider'></li>
+							<Link to='work' activeClassName='nav-link-active' className='nav-link'>WORK</Link>
+							<li className='nav-divider'></li>
+							<Link to='resume' activeClassName='nav-link-active' className='nav-link'>RESUME</Link>
+							</ul>
+					</div>
 				</div>
 			</div>
 		);
