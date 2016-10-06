49d1af1d49b6e4b71191c23948bc1cac1a9561f9
Rachel Rose Ulgado <rachel.ulgado@gmail.com>
2016-10-06 16:18:23 -0700

    fixing the last routing issue for mobile! 🎉

--- src/js/components/nav.js
+++ src/js/components/nav.js
@@ -8,11 +8,11 @@ export default class Nav extends Component {
 				<div className='nav-container-inner' id={this.props.currentPath}>
 					<div className='ul-container'>
 						<ul className='nav-links'>
-							<Link to='about' activeClassName='nav-link-active' className='nav-link'>ABOUT</Link>
+							<Link to='/about' activeClassName='nav-link-active' className='nav-link'>ABOUT</Link>
 							<li className='nav-divider'></li>
-							<Link to='work' activeClassName='nav-link-active' className='nav-link'>WORK</Link>
+							<Link to='/work' activeClassName='nav-link-active' className='nav-link'>WORK</Link>
 							<li className='nav-divider'></li>
-							<Link to='resume' activeClassName='nav-link-active' className='nav-link'>RESUME</Link>
+							<Link to='/resume' activeClassName='nav-link-active' className='nav-link'>RESUME</Link>
 						</ul>
 					</div>
 				</div>
